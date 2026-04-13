d51dd7f203fe8ea8ebaf8e47c92b8fa986b5ba2b
vinicirnne <viniciuscirne7@gmail.com>
2026-04-13 19:29:37 -0300

    feat: Adição do JWT no projeto

--- Back-End/src/app.js
+++ Back-End/src/app.js
@@ -1,10 +1,19 @@
 require('dotenv').config();
 
+const jwt = require('jsonwebtoken');
+
 // Verificação básica para garantir que as variáveis de ambiente estão carregadas
 if (!process.env.DB_NAME) {
   console.error("❌ ERRO: Arquivo .env não encontrado ou variáveis de banco faltando!");
   process.exit(1); // Para a execução do Node
 }
+
+// Verificação do JWT_SECRET
+if (!process.env.JWT_SECRET) {
+  console.error("❌ ERRO: Variável de ambiente JWT_SECRET não encontrada!");
+  process.exit(1);
+}
+
 
 const express = require('express');
 const app = express();
