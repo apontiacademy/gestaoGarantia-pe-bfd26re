3629c426a2225c4b1ef91e7334a93148a1d016ff
LUCAS <lucasts.2110@gmail.com>
2026-04-20 16:18:01 -0300

    add react router dom
    criação das rotas com routes

--- Front-End/src/App.tsx
+++ Front-End/src/App.tsx
@@ -1,10 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Splash from './Pages/Splash';
 import Login from './Pages/Login';
 
 function App() {
-
   return (
-    <Login />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Splash />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Navigate to="/login" />} />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
