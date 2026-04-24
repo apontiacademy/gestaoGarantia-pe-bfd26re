cc99254598141fbdd1da6437f1fcbdb07af01c06
Bruna <brunasilva.sms@gmail.com>
2026-04-24 15:50:22 -0300

    adicionar garantia

---
+++ Back-End/src/migrations/20260424184612-create-garantia.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Garantia', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      produto_id: {
+        type: Sequelize.INTEGER
+      },
+      prazo_dias: {
+        type: Sequelize.INTEGER
+      },
+      data_inicio: {
+        type: Sequelize.DATE
+      },
+      data_fim: {
+        type: Sequelize.DATE
+      },
+      tipo: {
+        type: Sequelize.STRING
+      },
+      data_cadastro: {
+        type: Sequelize.DATE
+      },
+      observacao: {
+        type: Sequelize.STRING
+      },
+      deletado_em: {
+        type: Sequelize.DATE
+      },
+      deletado_por: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Garantia');
+  }
+};
---
+++ Back-End/src/models/garantia.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Garantia extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Garantia.init({
+    produto_id: DataTypes.INTEGER,
+    prazo_dias: DataTypes.INTEGER,
+    data_inicio: DataTypes.DATE,
+    data_fim: DataTypes.DATE,
+    tipo: DataTypes.STRING,
+    data_cadastro: DataTypes.DATE,
+    observacao: DataTypes.STRING,
+    deletado_em: DataTypes.DATE,
+    deletado_por: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Garantia',
+  });
+  return Garantia;
+};
