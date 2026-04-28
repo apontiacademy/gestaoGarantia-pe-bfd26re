ab776a5222be78c24211e73aef474f4a37930d15
Bruna <brunasilva.sms@gmail.com>
2026-04-28 17:24:19 -0300

    atualizacao nome

--- Back-End/src/config/config.js
+++ Back-End/src/config/config.js
@@ -1,9 +1,41 @@
-require('dotenv').config();
+// require('dotenv').config();
+
+// module.exports = {
+//   "development": {
+//     "username": process.env.DB_USER,
+//     "password": process.env.DB_PASSWORD,
+//     "database": process.env.DB_NAME,
+//     "host": process.env.DB_HOST,
+//     "dialect": "postgres",
+//     "port": process.env.DB_PORT
+//   },
+//   "test": {
+//     "username": "seu_usuario",
+//     "password": "sua_senha",
+//     "database": "nome_do_banco_teste",
+//     "host": "127.0.0.1",
+//     "dialect": "postgres",
+//     "port": 5432
+//   },
+//   "production": {
+//     "use_env_variable": "DATABASE_URL",
+//     "dialect": "postgres",
+//     "dialectOptions": {
+//       "ssl": {
+//         "require": true,
+//         "rejectUnauthorized": false
+//       }
+//     }
+//   }
+// }
+
+// require('dotenv').config();
+require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 
 module.exports = {
   "development": {
     "username": process.env.DB_USER,
-    "password": process.env.DB_PASSWORD,
+    "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
     "dialect": "postgres",
@@ -28,3 +60,5 @@ module.exports = {
     }
   }
 }
+
+console.log('DB_USER:', process.env.DB_USER);
