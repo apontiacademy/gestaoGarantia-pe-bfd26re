7b9386722b1994ab9bcafe57f9e3b03005d4fc09
gabrifgaraujo <gabrifelipegf@gmail.com>
2026-05-22 21:55:51 -0300

    (fix): Nodemailer Port Config

--- Back-End/src/controllers/loginAuth.js
+++ Back-End/src/controllers/loginAuth.js
@@ -6,10 +6,12 @@ const { compararSenha } = require("../utils/hash");
 
 // Configuração do Nodemailer
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false, 
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
+    pass: process.env.EMAIL_PASS
   }
 });
 
