53f5c36ff3f23d9a05c503e700df80187618737f
Priscilla Marinho <priscillamarinho2005@gmail.com>
2026-06-16 11:07:54 -0300

    fix: correção para edição de garntias funcionar

--- Back-End/src/controllers/documentoFiscalController.js
+++ Back-End/src/controllers/documentoFiscalController.js
@@ -45,6 +45,52 @@ class DocumentoFiscalController {
 
         }
     }
+
+    async update(req, res) {
+        try {
+            const { produto_id } = req.params;
+            const {
+                cnpj_emissor,
+                valor,
+                quantidade,
+                valorInformado,
+                data_compra,
+                numero_nf,
+                serie_nota,
+                chave_acesso,
+                tipo
+            } = req.body;
+
+            const documento = await Documento_Fiscal.findOne({
+                where: { produto_id: Number(produto_id) }
+            });
+
+            if (!documento) {
+                return res.status(404).json({ erro: 'Documento fiscal não encontrado' });
+            }
+
+            const resultado = calcularValorTotal(valor, quantidade, valorInformado);
+
+            await documento.update({
+                cnpj_emissor,
+                valor_unitario: resultado.valor_unitario,
+                valor: resultado.valor_total,
+                quantidade: resultado.quantidade,
+                data_compra,
+                numero_nf,
+                serie_nota,
+                chave_acesso,
+                tipo
+            });
+
+            return res.json(documento);
+
+        } catch (error) {
+            return res.status(500).json({
+                erro: error.message
+            });
+        }
+    }
 }
 
 module.exports = new DocumentoFiscalController();
